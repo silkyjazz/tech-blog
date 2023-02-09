3f3972d6f12c4d4ecb78bd2d207f98b1ced42828
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-02-08 19:41:30 -0800

    set up main  handlebars template

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ const sequelize = require('./config/connection')
 
 //Sets up the express app
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
+
+//Sets up handlebars
+const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req,res) =>{
+    res.render('index')
+})
+
+app.get('/login',(req, res) =>{
+    res.render('login')
+})
+// app.use(require('./controllers/index'));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
